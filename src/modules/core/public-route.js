fbefb5c015c517efe491de0143332cb314fa623d
JsIqbal <javascriptiqbal@gmail.com>
2022-11-24 03:39:27 +0600

    Add Dashboard view

--- src/modules/core/public-route.js
+++ src/modules/core/public-route.js
@@ -6,16 +6,7 @@ function PublicRoute ({ children }) {
     console.log(loggedInUser);
     
     if (loggedInUser === "admin") {
-        return <Navigate to="/admin/dashboard" replace />
-    }
-    if (loggedInUser === "moderator") {
-        return <Navigate to="/moderator/dashboard" replace />
-    }
-    if (loggedInUser === "parent") {
-        return <Navigate to="/parent/dashboard" replace />
-    }
-    if (loggedInUser === "child") {
-        return <Navigate to="/child/dashboard" replace />
+        return <Navigate to="/dashboard" replace />
     }
     return children;
 }
